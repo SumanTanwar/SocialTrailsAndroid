7482d202ad9acc05df8c405306e565b9fe0aafbd
krupa <krupa@KRUPA>
2024-09-20 14:33:52 -0400

    initial project

---
+++ app/src/main/java/com/example/socialtrailsapp/adminpanel/DashBoardActivity.java
@@ -0,0 +1,26 @@
+package com.example.socialtrailsapp.adminpanel;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.socialtrailsapp.R;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.admin_activity_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
