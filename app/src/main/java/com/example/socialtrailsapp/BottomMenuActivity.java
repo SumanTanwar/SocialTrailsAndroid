e9d34f5be238bef38871d3f14541e2ffb689f53e
Barsha-RokaMagar <130592450+Barsha-RokaMagar@users.noreply.github.com>
2024-09-25 15:11:36 -0400

    bottom menu navigation user

---
+++ app/src/main/java/com/example/socialtrailsapp/BottomMenuActivity.java
@@ -0,0 +1,51 @@
+package com.example.socialtrailsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bottom_menu);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(R.id.navigation_dashboard == item.getItemId())
+                {
+                    startActivity(new Intent(BottomMenuActivity.this, MainActivity.class));
+                    return true;
+                }
+                else if(R.id.navigation_post == item.getItemId())
+                {
+                    startActivity(new Intent(BottomMenuActivity.this, MainActivity.class));
+                    return true;
+                }
+                else if(R.id.navigation_profile == item.getItemId())
+                {
+                    startActivity(new Intent(BottomMenuActivity.this, MainActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    }
