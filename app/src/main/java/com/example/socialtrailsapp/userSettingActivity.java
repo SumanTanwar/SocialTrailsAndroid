11260799d5bac1f6834448d1bc88d9aada3c8cf3
Suman Tanwar <stanwar2030@gmail.com>
2024-09-26 14:29:40 -0400

    Notification Activate

--- app/src/main/java/com/example/socialtrailsapp/userSettingActivity.java
+++ app/src/main/java/com/example/socialtrailsapp/userSettingActivity.java
@@ -9,12 +9,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.socialtrailsapp.Interface.OperationCallback;
 import com.example.socialtrailsapp.Utility.SessionManager;
@@ -27,17 +22,15 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class userSettingActivity extends BottomMenuActivity {
 
-    TextView txtLogout,txtprofileuser,txtchangepwd,txtdelprofile;
+    TextView txtLogout, txtprofileuser, txtchangepwd, txtdelprofile;
     private SessionManager sessionManager;
     UserService userService;
     FirebaseAuth mAuth;
     Switch switchNotify;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_user_setting);
         getLayoutInflater().inflate(R.layout.activity_user_setting, findViewById(R.id.container));
 
         txtLogout = findViewById(R.id.txtLogout);
@@ -49,139 +42,80 @@ public class userSettingActivity extends BottomMenuActivity {
         sessionManager = SessionManager.getInstance(this);
         userService = new UserService();
 
-
         if (sessionManager.userLoggedIn()) {
             txtprofileuser.setText(sessionManager.getUsername());
             switchNotify.setChecked(sessionManager.getNotificationStatus());
         }
 
-        txtchangepwd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(userSettingActivity.this, changePassword.class);
-                startActivity(intent);
-                finish();
-            }
+        txtchangepwd.setOnClickListener(view -> {
+            Intent intent = new Intent(userSettingActivity.this, changePassword.class);
+            startActivity(intent);
+            finish();
         });
 
-        txtLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sessionManager.logoutUser();
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(userSettingActivity.this, SignInActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        txtLogout.setOnClickListener(view -> {
+            sessionManager.logoutUser();
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(userSettingActivity.this, SignInActivity.class);
+            startActivity(intent);
+            finish();
         });
 
+        txtdelprofile.setOnClickListener(view -> {
+            new AlertDialog.Builder(userSettingActivity.this)
+                    .setTitle("Delete Account")
+                    .setMessage("Are you sure you want to delete your account?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null) {
+                            userService.deleteProfile(sessionManager.getUserID(), new OperationCallback() {
+                                @Override
+                                public void onSuccess() {
+                                    user.delete().addOnCompleteListener(task -> {
+                                        if (task.isSuccessful()) {
+                                            Utils.saveCredentials(userSettingActivity.this, "", false);
+                                            sessionManager.logoutUser();
+                                            FirebaseAuth.getInstance().signOut();
+                                            Intent intent = new Intent(userSettingActivity.this, SignInActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        } else {
+                                            userService.setbackdeleteProfile(sessionManager.getUserID());
+                                            Toast.makeText(userSettingActivity.this, "Delete profile failed, please try again later.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
 
-        txtdelprofile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                new AlertDialog.Builder(userSettingActivity.this)
-                        .setTitle("Delete Account")
-                        .setMessage("Are you sure you want to delete your account")
-                        .setPositiveButton("Yes",(dialog,which) -> {
-
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if(user != null)
-                            {
-                                userService.deleteProfile(sessionManager.getUserID(), new OperationCallback() {
-                                    @Override
-                                    public void onSuccess() {
-                                        user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-
-                                                if (task.isSuccessful())
-                                                {
-                                                    Utils.saveCredentials(userSettingActivity.this,"",false);
-                                                    sessionManager.logoutUser();
-                                                    FirebaseAuth.getInstance().signOut();
-                                                    Intent intent = new Intent(userSettingActivity.this,SignInActivity.class);
-                                                    startActivity(intent);
-                                                    finish();
-                                                }
-                                                else
-                                                {
-                                                    userService.setbackdeleteProfile(sessionManager.getUserID());
-                                                    Toast.makeText(userSettingActivity.this,
-                                                            "Delete profile failed, Pleasetry again later",
-                                                            Toast.LENGTH_SHORT).show();
-                                                }
-
-                                            }
-                                        });
-                                    }
-
-                                    @Override
-                                    public void onFailure(String errMessage) {
-
-                                        userService.setbackdeleteProfile(sessionManager.getUserID());
-                                        Toast.makeText(userSettingActivity.this,
-                                                "Delete profile failed, Pleasetry again later",
-                                                Toast.LENGTH_SHORT).show();
-
-                                    }
-                                });
-                            }
-                            else
-                            {
-                                Toast.makeText(userSettingActivity.this,
-                                        "Delete profile failed, Pleasetry again later",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-
-                        })
-                        .setNegativeButton("No",(dilog,which) -> dilog.dismiss())
-                        .show();
-            }
+                                @Override
+                                public void onFailure(String errMessage) {
+                                    userService.setbackdeleteProfile(sessionManager.getUserID());
+                                    Toast.makeText(userSettingActivity.this, "Delete profile failed, please try again later.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(userSettingActivity.this, "Delete profile failed, please try again later.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("No", (dialog, which) -> dialog.dismiss())
+                    .show();
         });
 
+        switchNotify.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            userService.setNotification(sessionManager.getUserID(), isChecked, new OperationCallback() {
+                @Override
+                public void onSuccess() {
+                    sessionManager.setNotificationStatus(isChecked);
+                    Toast.makeText(userSettingActivity.this,
+                            isChecked ? "Notifications turned ON" : "Notifications have been turned OFF. You can enable them again in the settings.",
+                            Toast.LENGTH_SHORT).show();
+                }
 
-
-//        txtdelprofile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                FirebaseUser user = mAuth.getCurrentUser();
-//                if(user !=  null) {
-//                    userService.deleteProfile(sessionManager.getUserID(), new OperationCallback() {
-//                        @Override
-//                        public void onSuccess() {
-//                            user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                @Override
-//                                public void onComplete(@NonNull Task<Void> task) {
-//                                    if (task.isSuccessful()) {
-//                                        Utils.saveCredentials(userSettingActivity.this,"", false);
-//                                        sessionManager.logoutUser();
-//                                        FirebaseAuth.getInstance().signOut();
-//                                        Intent intent = new Intent(userSettingActivity.this, SignInActivity.class);
-//                                        startActivity(intent);
-//                                        finish();
-//                                    } else {
-//                                        userService.setbackdeleteProfile(sessionManager.getUserID());
-//                                        Toast.makeText(userSettingActivity.this, "Delete Profile failed! Please try again later.", Toast.LENGTH_SHORT).show();
-//                                    }
-//                                }
-//                            });
-//                        }
-//
-//                        @Override
-//                        public void onFailure(String errMessage) {
-//                            userService.setbackdeleteProfile(sessionManager.getUserID());
-//                            Toast.makeText(userSettingActivity.this, "Delete Profile failed! Please try again later.", Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//                }
-//                else
-//                {
-//                    Toast.makeText(userSettingActivity.this, "Delete Profile failed! Please try again later.", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-
+                @Override
+                public void onFailure(String errMessage) {
+                    switchNotify.setChecked(sessionManager.getNotificationStatus());
+                    Toast.makeText(userSettingActivity.this, "Notification setting failed! Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
     }
 }
