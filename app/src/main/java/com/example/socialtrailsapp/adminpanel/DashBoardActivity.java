a150da3ae659832ef3b2a6b5f9e22dfb316a768c
Barsha-RokaMagar <130592450+Barsha-RokaMagar@users.noreply.github.com>
2024-09-29 22:09:29 -0400

    Added admin bottom navigation and related resources

--- app/src/main/java/com/example/socialtrailsapp/adminpanel/DashBoardActivity.java
+++ app/src/main/java/com/example/socialtrailsapp/adminpanel/DashBoardActivity.java
@@ -10,17 +10,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.socialtrailsapp.R;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class DashBoardActivity extends AdminBottomMenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.admin_activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.admin_activity_dashboard);
+        getLayoutInflater().inflate(R.layout.admin_activity_dashboard, findViewById(R.id.container));
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
