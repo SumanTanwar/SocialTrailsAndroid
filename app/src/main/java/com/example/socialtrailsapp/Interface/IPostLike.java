570d4adb8c030dfd496fa5d20344b3be91b40808
krupa <krupa@KRUPA>
2024-10-22 14:54:04 -0400

    user like list in each post

--- app/src/main/java/com/example/socialtrailsapp/Interface/IPostLike.java
+++ app/src/main/java/com/example/socialtrailsapp/Interface/IPostLike.java
@@ -2,8 +2,12 @@ package com.example.socialtrailsapp.Interface;
 
 import com.example.socialtrailsapp.ModelData.LikeResult;
 import com.example.socialtrailsapp.ModelData.PostLike;
+import com.example.socialtrailsapp.ModelData.Users;
+
+import java.util.List;
 
 public interface IPostLike {
     void likeandUnlikePost(String postId,String userId, DataOperationCallback<LikeResult> callback);
     void getPostLikeByUserandPostId(String postId, String userId, DataOperationCallback<PostLike> callback);
+    void getLikesForPost(String postId, DataOperationCallback<List<Users>> callback);
 }
